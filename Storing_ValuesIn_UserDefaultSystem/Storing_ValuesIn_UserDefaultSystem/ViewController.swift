38dfb3a1de735bb7d1697d1c84d1ecf2db2a13e3
c4arl0s <c.santiago.cruz@gmail.com>
2019-09-09 21:04:25 -0500

    Add project to the respository

---
+++ Storing_ValuesIn_UserDefaultSystem/Storing_ValuesIn_UserDefaultSystem/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Storing_ValuesIn_UserDefaultSystem
+//
+//  Created by Carlos Santiago Cruz on 9/9/19.
+//  Copyright © 2019 Carlos Santiago Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Storing_ValuesIn_UserDefaultSystem/Storing_ValuesIn_UserDefaultSystemTests/Storing_ValuesIn_UserDefaultSystemTests.swift
@@ -0,0 +1,34 @@
+//
+//  Storing_ValuesIn_UserDefaultSystemTests.swift
+//  Storing_ValuesIn_UserDefaultSystemTests
+//
+//  Created by Carlos Santiago Cruz on 9/9/19.
+//  Copyright © 2019 Carlos Santiago Cruz. All rights reserved.
+//
+
+import XCTest
+@testable import Storing_ValuesIn_UserDefaultSystem
+
+class Storing_ValuesIn_UserDefaultSystemTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
